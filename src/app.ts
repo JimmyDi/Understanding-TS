1b2b6b9c13cae178cf67a50211dde9fcaaaf1282
liudi25ece@outlook.com <liudi25ece@outlook.com>
2022-08-28 11:08:54 +0800

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,46 @@
-// code goes here!
-const userName = "Max";
-// useName = "Maximilian";
+interface AddFn {
+    (a: number, b: number): number;
+}
 
-let age = 30;
-age = 29;
+// type AddFn = (a: number, b: number) => number;
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
+interface Named {
+    readonly name?: string;
+    outputName?: string;
+}
+
+interface Greatable extends Named {
+    greet(phrase: string): void;
+}
+
+class Person implements Greatable {
+    name?: string;
+    age = 30;
+
+    constructor(n: string = '') {
+        if(n) {
+            this.name = n;
+        }
+    }
+
+    greet(phrase: string): void {
+        if(this.name) {
+            console.log(phrase + ' ' + this.name);
+        } else  {
+            console.log('Hi!');
+        }
+    }
+}
+
+let user1: Greatable;
+user1 = new Person();
+
+user1.greet('Hi there - I am');
+
+console.log(user1);
